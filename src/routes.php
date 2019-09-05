4574c52db13a058a3b3d4345554b3914938c912f
f.stamatoska <f.stamatoska@blexr.com>
2019-09-05 19:33:16 +0200

    Change path in routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::get('/api/unsplash/authorization', 'Frosinas/Unsplash/UnsplashController@authorization');
+Route::get('/api/unsplash/authorization', 'Frosinas\Unsplash\UnsplashController@authorization');
